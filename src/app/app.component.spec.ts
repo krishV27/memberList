aa4b85062df27aacb7b940ae004fc81ac228528d
krishna <krishna@appslab.in>
2022-03-27 21:05:22 +0100

    fixed title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ShowMemberList'`, () => {
+  it(`should have as title 'Members List'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ShowMemberList');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ShowMemberList app is running!');
+    expect(app.title).toEqual('Members List');
   });
 });
